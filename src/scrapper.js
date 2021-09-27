bf029d8e16797a59cb59ad8fa04640198cb402a3
ThomasHubert1 <famille.thubert@gmail.com>
2021-09-27 22:18:31 +0200

    Added the export in .csv

--- src/scrapper.js
+++ src/scrapper.js
@@ -23,10 +23,13 @@ class contest{
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 
+
 async function scrappPage(discipline, ligue, dateStart, dateEnd, pageMax, isLogTrue = false) {
     console.log("Initiating scrapping...");
     
-    
+    fs.writeFile('../ressources/info_concours.csv', "StartDate,EndDate,Lieu,mandat,TypeEpreuve,Departement,Distance,Etat", (err) => {
+        if (err) throw err;
+    })
     var countdown = 0
 
     for(let page = 1; page < pageMax + 1; page++){
@@ -47,6 +50,8 @@ async function scrappPage(discipline, ligue, dateStart, dateEnd, pageMax, isLogT
                     if (err) throw err;
                 })
                 */
+
+                
                const map_info = new Map();
 
                 contestTable.each(function() {
@@ -77,12 +82,37 @@ async function scrappPage(discipline, ligue, dateStart, dateEnd, pageMax, isLogT
                         
                         nbr_info++;
                     })
-                    console.log(map_info)
-                    console.log(`nombre d'info : ${nbr_info}`);
+
+                    //process datetime
+                    var StartDate = ""
+                    var EndDate = ""
+                    var datetomodify = map_info.get("Date");
+                    arrayDate = datetomodify.split(' ');
+                    if(arrayDate.length> 3){
+                        StartDate = arrayDate[1];
+                        EndDate = arrayDate[arrayDate.length-1];
+                    } else {
+                        StartDate = arrayDate[1]
+                        EndDate = arrayDate[1]
+                    }
+                    map_info.set("DateStart",StartDate)
+                    map_info.set("DateEnd", EndDate)
+
+                    //process discipline
+                    var discipline = map_info.get("Discipline")
+                    slice_discipline = discipline.split('-')
+                    new_discipline = slice_discipline[0].trim()
+                    console.log(new_discipline)
+                    map_info.set("Discipline_simple",new_discipline)
+                
+                    // write in the file
+                    console.log(map_info)    
+                    fs.appendFileSync('../ressources/info_concours.csv', `${map_info.get("DateStart")},${map_info.get("DateEnd")},${map_info.get("Lieu")},mandat,${map_info.get("Discipline_simple")}, ${map_info.get("Département")}, distance, ${map_info.get("État")}\n`);
+                    //console.log(`nombre d'info : ${nbr_info}`);
                 })
             })
     }
     console.log(`There are ${countdown} contest(s)`)
 }
 
-scrappPage("S", "CR08", "2021-09-18","2021-12-31", 1);
+scrappPage("S", "CR08", "2021-09-18","2021-12-31", 5);
